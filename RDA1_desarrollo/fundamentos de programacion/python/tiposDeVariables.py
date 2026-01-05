2b32090dd07a8a03aa6bc7132a106bce628de997
Usuario <usuario@Alejandro-Revilla-2.local>
2026-01-04 21:15:17 -0500

    inicio

---
+++ RDA1_desarrollo/fundamentos de programacion/python/tiposDeVariables.py
@@ -0,0 +1,29 @@
+#declaracion de variables
+edad = 30
+altura = 1.75
+numero_complejo = 3+2j
+es_estudiante = True
+nombre = "Juan Perez"
+
+#operaciones basicas
+print("Mi edad es:", edad)
+print("Mi altura es:", altura, "metros")
+print("Un numero complejo es:", numero_complejo)
+print("Soy estudiante...? :", es_estudiante)
+print("Mi nombre es:", nombre)
+
+#concatenacion de cadenas 
+saludo = "hola, " + nombre + "!"
+print(saludo)
+
+#conversion de tipos
+edad_como_cadena = str(edad)
+print("Mi edad en cadena es", edad_como_cadena)
+
+#prueba de edad
+edad = edad+5
+print("la edad es:", edad)
+
+#prueba 2
+edad_como_cadena = edad_como_cadena + "-" +"5"
+print("edad como cadena", edad_como_cadena)
